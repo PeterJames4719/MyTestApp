254169c0f7faef9055f8af81d9a3720fe0fa5023
bing5 <18513213028@163.com>
2021-06-24 10:59:00 +0800

    first commit

---
+++ MyTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTestApp
+//
+//  Created by 伍兵 on 2021/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
